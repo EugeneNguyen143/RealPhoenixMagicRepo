0870e0e71e9ad4a0b857f999409da2b83e42c1ab
Administrator <Administrator@PC240>
2020-06-11 14:01:23 +0700

    Date/Time Issue with CourseController

--- GiantSchool/Controllers/CoursesController.cs
+++ GiantSchool/Controllers/CoursesController.cs
@@ -8,10 +8,21 @@ namespace GiantSchool.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
         // GET: Courses
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList(),
+                Heading = "Add Course"
+            };
+            return View(viewModel);
         }
     }
 }
