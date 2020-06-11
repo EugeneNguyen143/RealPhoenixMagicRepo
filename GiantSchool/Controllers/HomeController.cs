d2bb068e6779fb4020ee8acfa82dff86e47e46ef
Administrator <Administrator@PC240>
2020-06-11 16:13:48 +0700

    Class Finalize

--- GiantSchool/Controllers/HomeController.cs
+++ GiantSchool/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace GiantSchool.Controllers
 {
@@ -17,7 +18,11 @@ namespace GiantSchool.Controllers
         }
         public ActionResult Index()
         {
-            return View();
+            var upcomingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcomingCourses);
         }
 
         public ActionResult About()
